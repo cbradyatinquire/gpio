a2a707b7f3bc2082e1d82cd7c801359fa6a7138a
Corey Brady <cbrady@inquirelearning.com>
2013-04-05 15:38:43 -0500

    first attempt at all read-write functionality

--- src/src/gpio/GpioExtension.java
+++ src/src/gpio/GpioExtension.java
@@ -1,6 +1,7 @@
 package gpio;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -23,6 +24,10 @@ public class GpioExtension extends DefaultClassManager {
 	static final String pinDir = "/sys/devices/virtual/misc/gpio/pin/";
 	static final String modeDir= "/sys/devices/virtual/misc/gpio/mode/";
 	
+	static final String analogPinDir = "/proc";
+	static final String pwmPinDir = "/sys/class/leds/";
+	
+	
 	
 	static final String UNINITIALIZED = "uninitialized";
 	static final String WRITE = "output";
@@ -34,6 +39,20 @@ public class GpioExtension extends DefaultClassManager {
 		legalModes.add(READ);
 	}
 	
+	static final String[] avalableAnalogs = {"adc0", "adc1", "adc2", "adc3", "adc4", "adc5" };
+	static final String[] availablePWMs = {"pwm0", "pwm1", "pwm2", "pwm3", "pwm4", "pwm5"};
+	static final ArrayList<String> analogPinList = new ArrayList<String>();
+	static final ArrayList<String> pwmPinList = new ArrayList<String>();
+	static {
+		for (String ap : avalableAnalogs)
+		{
+			analogPinList.add( ap );
+		}
+		for (String pp : availablePWMs)
+		{
+			pwmPinList.add( pp );
+		}
+	}
 	
 	static final String[] availablePins = {"gpio0","gpio1","gpio2","gpio3","gpio4","gpio5","gpio6","gpio7",
             "gpio8", "gpio9", "gpio10", "gpio11", "gpio12", "gpio13",
@@ -56,7 +75,9 @@ public class GpioExtension extends DefaultClassManager {
 		pm.addPrimitive("led-off", new LedOff() );
 		pm.addPrimitive("set-pin-mode", new SetPinMode() );
 		pm.addPrimitive("digital-write", new DigitalWrite() );
-//		pm.addPrimitive("digital-read", new DigitalRead() );
+		pm.addPrimitive("digital-read", new DigitalRead() );
+		pm.addPrimitive("pwm-write", new PWMWrite() );
+		pm.addPrimitive("analog-read", new AnalogRead() );
 		pm.addPrimitive("get-pin-info", new GetPinInfo() );
 	}
 
@@ -176,6 +197,142 @@ public class GpioExtension extends DefaultClassManager {
 	
 	
 	
+	public static class AnalogRead extends DefaultReporter {
+		public Syntax getSyntax() {
+			return Syntax.reporterSyntax(new int[] { Syntax.StringType(),
+					 }, Syntax.NumberType() );
+		}
+		
+		@Override
+		public Object report(Argument[] arg, Context ctxt)
+		throws ExtensionException, LogoException {
+
+			String pin = arg[0].getString();
+			String contents = "";
+			double toreturn = -1.0;
+			if ( analogPinList.contains(pin) )
+			{
+
+				try
+				{
+					File f = new File( analogPinDir + pin );
+					FileInputStream fis = new FileInputStream( f );
+					int contint;
+					while ((contint = fis.read()) != -1)
+					{
+						contents += (char)contint;
+					}
+					toreturn = Double.valueOf(contents);
+					fis.close();
+				}
+				catch (Exception e)
+				{
+					e.printStackTrace();
+					throw new ExtensionException( "An exception occurred in trying to read from analog pin " + pin + ".");
+				}
+
+
+
+			}	
+			else
+			{
+				throw new ExtensionException("Analog pin " + pin + " is not defined for this interface to pcDuino.");
+			}
+
+			return toreturn;
+		}
+	}
+	
+	
+	
+	public static class DigitalRead extends DefaultReporter {	
+		public Syntax getSyntax() {
+			return Syntax.reporterSyntax(new int[] { Syntax.StringType(),
+					 }, Syntax.NumberType() );
+		}
+		
+		//THIS is overkill -- i'm reading just one byte (the character for "1" or "0")
+		@Override
+		public Object report(Argument[] arg, Context ctxt)
+				throws ExtensionException, LogoException {
+
+			String pin = arg[0].getString();
+			String contents = "";
+			double toreturn = -1.0;
+			if ( pinList.contains(pin) )
+			{
+				String mode = pinStates.get(pin);
+				if ( mode.equals(READ) )
+				{
+					
+						try
+						{
+							File f = new File( pinDir + pin );
+							FileInputStream fis = new FileInputStream( f );
+							int contint;
+							while ((contint = fis.read()) != -1)
+							{
+								contents += (char)contint;
+							}
+							toreturn = Double.valueOf(contents);
+							fis.close();
+						}
+						catch (Exception e)
+						{
+							e.printStackTrace();
+							throw new ExtensionException( "An exception occurred in trying to read from pin " + pin + ".");
+						}
+					
+				}
+				else
+				{
+					throw new ExtensionException("Pin " + pin + " is not set to READ mode.");
+				}
+			}	
+			else
+			{
+				throw new ExtensionException("Pin " + pin + " is not defined for this interface to pcDuino.");
+			}
+			
+			return toreturn;
+		}
+		
+	}
+	
+	
+	public static class PWMWrite extends DefaultCommand {
+		public Syntax getSyntax() {
+			return Syntax.commandSyntax(new int[] { Syntax.StringType(),
+					Syntax.StringType() });
+		}
+
+		@Override
+		public void perform(Argument[] arg, Context arg1)
+				throws ExtensionException, LogoException {
+			String pin = arg[0].getString();
+			String value = arg[1].getString();
+			if ( pwmPinList.contains(pin) )
+			{
+				try
+				{
+					File f = new File( pwmPinDir + pin );
+					FileOutputStream fos = new FileOutputStream( f );
+					fos.write( value.getBytes() );
+					fos.close();
+				}
+				catch (Exception e)
+				{
+					e.printStackTrace();
+					throw new ExtensionException( "An exception occurred in trying to set pin " + pin + " to value " + value + ".");
+				}
+			}	
+			else
+			{
+				throw new ExtensionException("PWM Pin " + pin + " is not defined for this interface to pcDuino.");
+			}
+		}
+	}
+	
 	
 	public static class DigitalWrite extends DefaultCommand {
 		public Syntax getSyntax() {
@@ -228,6 +385,8 @@ public class GpioExtension extends DefaultClassManager {
 			
 		}
 	}
+	
+	
 	
 	
 	
