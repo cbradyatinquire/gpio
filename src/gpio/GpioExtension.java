630ea61c9b01320e6d4be5e7b9eb7a92424d2edf
Corey Brady <cbrady@inquirelearning.com>
2014-07-06 18:17:19 -0500

    test prims for new PWM

--- src/gpio/GpioExtension.java
+++ src/gpio/GpioExtension.java
@@ -24,11 +24,15 @@ public class GpioExtension extends DefaultClassManager {
 	static final String pinDir = "/sys/devices/virtual/misc/gpio/pin/";
 	static final String modeDir= "/sys/devices/virtual/misc/gpio/mode/";
 	
+	static final String pwmDir = "/sys/devices/virtual/misc/pwmtimer/";
+	static final String pwmEnable = pwmDir + "enable/";
+	static final String pwmFreq = pwmDir + "freq/";
+	static final String pwmLevel = pwmDir + "level/";
+	
 	static final String analogPinDir = "/proc/";
-	static final String pwmPinDir = "/sys/class/leds/";
 	
 	
-	
+
 	static final String UNINITIALIZED = "uninitialized";
 	static final String WRITE = "output";
 	static final String READ = "input";
@@ -39,7 +43,39 @@ public class GpioExtension extends DefaultClassManager {
 		legalModes.add(READ);
 	}
 	
+	//adc0 and adc1 -- 0 to 2 volts, readings with 6 bits --> 0 to 63 is reading.
+	//adc2 to adc5 -- 0 to 3.3 volts, readings with 12 bits --> 0 to 4095
 	static final String[] avalableAnalogs = {"adc0", "adc1", "adc2", "adc3", "adc4", "adc5" };
+	
+	/*
+	 * PWM filesystem map (assuming sysfs is insalled...)
+	 * 
+/sys/devices/virtual/misc/pwmtimer/freq/pwmX (r/w min_freq to max_freq )
+/sys/devices/virtual/misc/pwmtimer/freq_range/pwmX (read only: freq range)
+/sys/devices/virtual/misc/pwmtimer/level/pwmX (r/w 0 to max_level )
+/sys/devices/virtual/misc/pwmtimer/enable/pwmX (r/w 1: enable, 0:disable)
+/sys/devices/virtual/misc/pwmtimer/max_level/pwmX (read only)
+	...where X should be one of 5/6/3/9/10/11
+	 *
+	 *
+	 *usage:
+	 *
+1) set pin mode function to pwm mode
+PWM5/6
+ echo 2 > /sys/devices/virtual/misc/gpio/mode/gpio5
+ echo 2 > /sys/devices/virtual/misc/gpio/mode/gpio6
+PWM3/9/10/11
+ echo 1 > /sys/devices/virtual/misc/gpio/mode/gpio3
+ echo 1 > /sys/devices/virtual/misc/gpio/mode/gpio9
+ echo 1 > /sys/devices/virtual/misc/gpio/mode/gpio10
+ echo 1 > /sys/devices/virtual/misc/gpio/mode/gpio11
+2) disable pwm before changing freq
+ echo 0 > /sys/devices/virtual/misc/pwmtimer/enable/pwmX
+3) set freq ( min_freq <= freq <= max_freq )
+ echo FREQ > /sys/devices/virtual/misc/pwmtimer/freq/pwmX
+ 
+NOTE: you can get freq first: cat /sys/devices/virtual/misc/pwmtimer/freq_range/pwmX
+	 */
 	static final String[] availablePWMs = {"pwm0/brightness", "pwm1/brightness", "pwm2/brightness", "pwm3/brightness", "pwm4/brightness", "pwm5/brightness"};
 	static final ArrayList<String> analogPinList = new ArrayList<String>();
 	static final ArrayList<String> pwmPinList = new ArrayList<String>();
@@ -70,13 +106,13 @@ public class GpioExtension extends DefaultClassManager {
 	
 	@Override
 	public void load(PrimitiveManager pm) throws ExtensionException {
-		pm.addPrimitive("test", new TestPrimitive()  );
+		pm.addPrimitive("test1", new TestPWMPrimitive()  );
+		pm.addPrimitive("test2", new TestPWMPrimitive2()  );
 		pm.addPrimitive("led-on", new LedOn() );
 		pm.addPrimitive("led-off", new LedOff() );
 		pm.addPrimitive("set-pin-mode", new SetPinMode() );
 		pm.addPrimitive("digital-write", new DigitalWrite() );
 		pm.addPrimitive("digital-read", new DigitalRead() );
-		pm.addPrimitive("pwm-write", new PWMWrite() );
 		pm.addPrimitive("analog-read", new AnalogRead() );
 		pm.addPrimitive("get-pin-info", new GetPinInfo() );
 	}
@@ -96,12 +132,98 @@ public class GpioExtension extends DefaultClassManager {
 		}
 	}
 	
-	public static class TestPrimitive extends DefaultReporter {
+	public static class TestPWMPrimitive extends DefaultReporter {
 
 		@Override
 		public Object report(Argument[] arg0, Context arg1)
 				throws ExtensionException, LogoException {
-			return "Hello";
+			try {
+				File fmode = new File( modeDir + "gpio5" );
+				FileOutputStream modefos = new FileOutputStream( fmode );
+				modefos.write( "2".getBytes() );
+				modefos.close();
+
+				File fenable = new File( pwmEnable + "gpio5" );
+				FileOutputStream enableFOS = new FileOutputStream( fenable );
+				enableFOS.write( "0".getBytes() );
+				enableFOS.close();
+				
+				File ffreq = new File( pwmEnable + "gpio5" );
+				FileOutputStream freqfos = new FileOutputStream( ffreq );
+				freqfos.write( "195".getBytes() );
+				freqfos.close();
+				
+				fenable = new File( pwmEnable + "gpio5" );
+				enableFOS = new FileOutputStream( fenable );
+				enableFOS.write( "1".getBytes() );
+				enableFOS.close();
+				
+				File flevel = new File( pwmLevel + "gpio5" );
+				FileOutputStream levelfos = new FileOutputStream( flevel );
+				levelfos.write( "1".getBytes() );
+				levelfos.close();
+				
+				fenable = new File( pwmEnable + "gpio5" );
+				enableFOS = new FileOutputStream( fenable );
+				enableFOS.write( "0".getBytes() );
+				enableFOS.close();
+			} catch (FileNotFoundException fnfe) {
+				fnfe.printStackTrace();
+			} catch (IOException ioe ) {
+				ioe.printStackTrace();
+			}
+			
+			
+			return "PWM5 set to level 1";
+		}
+		
+	}
+	
+	public static class TestPWMPrimitive2 extends DefaultReporter {
+
+		@Override
+		public Object report(Argument[] arg0, Context arg1)
+				throws ExtensionException, LogoException {
+			try {
+				File fmode = new File( modeDir + "gpio5" );
+				FileOutputStream modefos = new FileOutputStream( fmode );
+				modefos.write( "2".getBytes() );
+				modefos.close();
+
+				File fenable = new File( pwmEnable + "gpio5" );
+				FileOutputStream enableFOS = new FileOutputStream( fenable );
+				enableFOS.write( "0".getBytes() );
+				enableFOS.close();
+				
+				File ffreq = new File( pwmEnable + "gpio5" );
+				FileOutputStream freqfos = new FileOutputStream( ffreq );
+				freqfos.write( "195".getBytes() );
+				freqfos.close();
+				
+				fenable = new File( pwmEnable + "gpio5" );
+				enableFOS = new FileOutputStream( fenable );
+				enableFOS.write( "1".getBytes() );
+				enableFOS.close();
+				
+				File flevel = new File( pwmLevel + "gpio5" );
+				FileOutputStream levelfos = new FileOutputStream( flevel );
+				levelfos.write( "128".getBytes() );
+				levelfos.close();
+				
+
+				fenable = new File( pwmEnable + "gpio5" );
+				enableFOS = new FileOutputStream( fenable );
+				enableFOS.write( "0".getBytes() );
+				enableFOS.close();
+	
+			} catch (FileNotFoundException fnfe) {
+				fnfe.printStackTrace();
+			} catch (IOException ioe ) {
+				ioe.printStackTrace();
+			}
+			
+			
+			return "PWM5 set to level 128";
 		}
 		
 	}
@@ -314,38 +436,7 @@ public class GpioExtension extends DefaultClassManager {
 	}
 	
 	
-	public static class PWMWrite extends DefaultCommand {
-		public Syntax getSyntax() {
-			return Syntax.commandSyntax(new int[] { Syntax.StringType(),
-					Syntax.StringType() });
-		}
 
-		@Override
-		public void perform(Argument[] arg, Context arg1)
-				throws ExtensionException, LogoException {
-			String pin = arg[0].getString();
-			String value = arg[1].getString();
-			if ( pwmPinList.contains(pin) )
-			{
-				try
-				{
-					File f = new File( pwmPinDir + pin );
-					FileOutputStream fos = new FileOutputStream( f );
-					fos.write( value.getBytes() );
-					fos.close();
-				}
-				catch (Exception e)
-				{
-					e.printStackTrace();
-					throw new ExtensionException( "An exception occurred in trying to set pin " + pin + " to value " + value + ".");
-				}
-			}	
-			else
-			{
-				throw new ExtensionException("PWM Pin " + pin + " is not defined for this interface to pcDuino.");
-			}
-		}
-	}
 	
 	
 	public static class DigitalWrite extends DefaultCommand {
